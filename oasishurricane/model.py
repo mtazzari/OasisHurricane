24df4c8a19a9dfc208c35d27a38df77af3149c96
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2021-11-03 14:31:38 +0000

    [model] First printouts (prep to logging)

--- oasishurricane/model.py
+++ oasishurricane/model.py
@@ -18,41 +18,51 @@ def get_rng(seed=None):
 def simulate(florida_landfall_rate, florida_mean, florida_stddev,
              gulf_landfall_rate, gulf_mean, gulf_stddev, num_monte_carlo_samples,
              **kwargs):
-    """
-    Simulate
+        """
+        Simulate
 
-    :param florida_landfall_rate:
-    :param florida_mean:
-    :param florida_stddev:
-    :param gulf_landfall_rate:
-    :param gulf_mean:
-    :param gulf_stddev:
-    :param num_monte_carlo_samples:
+        :param florida_landfall_rate:
+        :param florida_mean:
+        :param florida_stddev:
+        :param gulf_landfall_rate:
+        :param gulf_mean:
+        :param gulf_stddev:
+        :param num_monte_carlo_samples:
 
-    :return: Mean annual losses
+        :return: Mean annual losses
 
-    """
-    rng_seed = kwargs.get('rng_seed', None)
+        """
+        rng_seed = kwargs.get('rng_seed', None)
 
-    # get a new random number generator
-    rng = get_rng(rng_seed)
+        # get a new random number generator
+        rng = get_rng(rng_seed)
 
-    tot_loss = 0
-    Nyears = 100
+        tot_loss = 0
 
-    for i in range(num_monte_carlo_samples):
-        year_loss = 0
+        for i in range(num_monte_carlo_samples):
+            print(f"*** YEAR {i}")
 
-        fl_events = rng.poisson(lam=florida_landfall_rate, size=1)
-        for j in range(fl_events):
-            year_loss += rng.lognormal(florida_mean, florida_stddev)
+            fl_events = rng.poisson(lam=florida_landfall_rate, size=1)[0]
+            print(fl_events)
+            fl_loss = 0
+            for j in range(fl_events):
+                fl_loss += rng.lognormal(florida_mean, florida_stddev)
+                print(fl_loss)
 
-        gulf_events = rng.poisson(lam=gulf_landfall_rate, size=1)
-        for k in range(gulf_events):
-            year_loss += rng.lognormal(gulf_mean, gulf_stddev)
+            gulf_events = rng.poisson(lam=gulf_landfall_rate, size=1)[0]
+            print(gulf_events)
+            gulf_loss = 0
+            for k in range(gulf_events):
+                gulf_loss += rng.lognormal(gulf_mean, gulf_stddev)
+                print(gulf_loss)
 
-        tot_loss += year_loss
+            year_loss = fl_loss + gulf_loss
 
-    mean_loss = tot_loss / Nyears
+            tot_loss += year_loss
+            print(f"TOT LOSS: {tot_loss}")
 
-    return mean_loss
+        mean_loss = tot_loss / num_monte_carlo_samples
+
+        print(f"MEAN LOSS: {mean_loss}")
+
+        return mean_loss
